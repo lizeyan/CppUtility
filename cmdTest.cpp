5b488db22fa2377f108fe23d8af941a26402f07f
zy-li14 <zy-li14@hotmail.com>
2016-07-12 23:00:07 +0800

    command init

---
+++ cmdTest.cpp
@@ -0,0 +1,12 @@
+#include "command.hpp"
+#include <string>
+#include <iostream>
+int main (int argc, char** argv)
+{
+    Utility::Command command;
+    command.add<std::string> ("protocal", 's', "transfer protocol", true, "http");
+    command.add<std::string> ("domain", 'd', "domain", false);
+    command.add<int> ("port", 'p', "port", true, 80, [] (int x) -> bool{return x > 0;});
+    command.parse (argc, argv);
+    std::cout << command.get<std::string> ("protocal") << "://" << command.get<std::string> ("domain") << ":" << command.get<int> ("port") << std::endl;
+}
